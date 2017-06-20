506143b33fa8ae309fa74ec9e930b62d58719399
samuel ogundipe <ayoogundipe2005@gmail.com>
2017-06-20 08:32:11 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app"> 
+    <div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <hello></hello>
+        </div>
+        <div class="col-md-6">
+           <done></done>
+        </div>
+    </div>
+</div>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import Done from './components/Done'
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+    Done
+  }
+}
+</script>
+
